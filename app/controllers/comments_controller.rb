4e23b36cc2c096cf200df5a26db25099e3ab8ac6
Stephen Paul Suarez <devpopol@gmail.com>
2012-01-29 06:48:58 +0800

    adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  
+  def create
+    Rails.logger.debug(([Share.config.endpoint, 'sources', params[:post_id], 'comments.json'].join('/') + "?api_key=" + Share.config.api_key))
+    response = RestClient.post(([Share.config.endpoint, 'sources', params[:post_id], 'comments.json'].join('/') + "?api_key=" + Share.config.api_key), {
+      :comment => (params[:comment].merge(:source_id => params[:post_id])), 
+      :dimension_identifiers => params[:app_id]
+    })
+    Rails.logger.debug(response.inspect)
+    render :text => response.body
+    
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :current_app
   
   def show
-    @source_id = params[:id] #this is actually the source identifier
+    @post_id = params[:id] #this is actually the source identifier
   end
   
   def current_app
@@ -12,9 +12,12 @@ class PostsController < ApplicationController
   def create
     #[Share.config.endpoint, 'sources.json'].join('/') + "?api_key=" + Share.config.api_key
     response = RestClient.post(([Share.config.endpoint, 'sources.json'].join('/') + "?api_key=" + Share.config.api_key), {
-      :source => params[:source], :dimension_identifiers => params[:dimension_identifiers]
+      :source => params[:source], :dimension_identifiers => @app.id
     })
     
     render :text => response.body
+  end
+  
+  def create_comment
   end
 end
