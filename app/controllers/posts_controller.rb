f1d0d36e439c4ce272fca7fae23501f22ae66230
Stephen Paul Suarez <devpopol@gmail.com>
2012-01-28 10:09:08 +0800

    various inputs from the server

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  before_filter :current_app
+  
+  def show
+    @source_id = params[:id] #this is actually the source identifier
+  end
+  
+  def current_app
+    @app ||= App.find(params[:app_id])
+  end
 end
