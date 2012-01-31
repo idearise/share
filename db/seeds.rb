811104b682ff5388fdba9c23cdb1823c1c01cdc8
roberto <roberto.rabe@idearise.com>
2012-01-30 23:38:39 -0500

    getting ready for demo deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ user4.save
 # FIXME Tried to use find_or_initialize_by_name instead of App.new to work around a weird
 # validates_uniqueness_of error, but there's still an issue with creating apps 2 to 4!
 app1 = App.find_or_initialize_by_name("example.com")
-app1.website = "http://www.example.com"
+app1.website = "www.example.com"
 app1.about = "This is just an example of how apps in inthisapp might be created"
 app1.platforms = platforms.values_at(0, 1)
 app1.user_id = user1.id
@@ -40,7 +40,7 @@ app1.user_id = user1.id
 app1.save_new_by(user1.id, '127.0.0.1')
 
 app2 = App.find_or_initialize_by_name("Android app") 
-app2.website = "http://www.android.com"
+app2.website = "www.android.com"
 app2.about = "Android example app description needs to be at least 32 characters in length!"
 app2.platforms = platforms.values_at(0, 1)
 app2.user_id = user2.id
@@ -48,7 +48,7 @@ app2.user_id = user2.id
 app2.save_new_by(user2.id, '127.0.0.1')
 
 app3 = App.find_or_initialize_by_name("iOS app")
-app3.website = "http://www.apple.com"
+app3.website = "www.apple.com"
 app3.about = "iOS example app description needs to be at least 32 characters in length!"
 app3.platforms = platforms.values_at(0, 2)
 app3.user_id = user3.id
@@ -56,7 +56,7 @@ app3.user_id = user3.id
 app3.save_new_by(user3.id, '127.0.0.1')
 
 app4 = App.find_or_initialize_by_name("Windows Phone app")
-app4.website = "http://www.microsoft.com"
+app4.website = "www.microsoft.com"
 app4.about = "Windows Phone app example description needs to be at least 32 characters in length!"
 app4.platforms = platforms.values_at(0, 3)
 app4.user_id = user4.id
