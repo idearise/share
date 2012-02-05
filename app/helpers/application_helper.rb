9fba9f029a7bf402f0e64aa8f079dfc89c91956c
roberto <roberto.rabe@idearise.com>
2012-02-05 16:54:35 -0500

    minor updates to layouts/views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
 	def markdown(text)
+		if !text.blank?
 	    redcarpet = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
-						:space_after_headers => true,
-						:fenced_code_blocks => true, 
-						:autolink => true, 
-						:no_intra_emphasis => false,
-						:strikethrough => true, 
-						:superscripts => true, 
-						:tables => true, 
-						:lax_html_blocks => true)
-    	redcarpet.render(text).html_safe
+										:space_after_headers => true,
+										:fenced_code_blocks => true, 
+										:autolink => true, 
+										:no_intra_emphasis => false,
+										:strikethrough => true, 
+										:superscripts => true, 
+										:tables => true, 
+										:lax_html_blocks => true)
+	  	redcarpet.render(text).html_safe
+	  end
 	end
 end
