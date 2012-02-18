bfa053df36380049d31bab94801754db367c332b
Stephen Paul Suarez <devpopol@gmail.com>
2012-02-18 15:04:06 +0800

    javscript stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,13 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
+
 //= require guard
-// #require jquery # already include Google CDN jQuery in application.html.erb
 //= require jquery_ujs
 //= require handlebars
 //= require jquery_nested_form
-//= require_tree .
+//= require sisyphus.min
+//= require jquery.markitup
+//= require set
+//= require bootstrap.min 
 
-$(function() {
-	$(".markitup").markItUp(mySettings);
-});
+//= require client
+//= require users
+
---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,4 @@
+
+$(function() {
+	$(".markitup").markItUp(mySettings);
+});
